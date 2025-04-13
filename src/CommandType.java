a65b18919cdb19d0260a6f58cc34fa84afa84746
Roman <grechoroma@gmail.com>
2025-04-13 15:09:48 +0300

    added CommandType enum

---
+++ src/CommandType.java
@@ -0,0 +1,20 @@
+public enum CommandType {
+    ENCRYPT("ENCRYPT"),
+    DECRYPT("DECRYPT"),
+    BRUTE_FORCE("BRUTE_FORCE");
+
+    private final String displayName;
+
+    CommandType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
+}
